6150db7dbebff4c1fc5227f43a748662c6cb470d
cqlyj <luoyingjie0721@gmail.com>
2024-04-18 04:32:59 +0800

    Add jobTypes and locationTypes constants to job-types.ts

---
+++ src/lib/job-types.ts
@@ -0,0 +1,10 @@
+export const jobTypes = [
+  "Full-time",
+  "Part-time",
+  "Contract",
+  "Volunteer",
+  "Internship",
+  "Temporary",
+];
+
+export const locationTypes = ["Remote", "On-site", "Hybrid"];
