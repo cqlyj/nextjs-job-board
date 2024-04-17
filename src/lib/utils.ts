6d8c1fd1fe617fe30a05c0117fe49ec0c788b2c9
cqlyj <luoyingjie0721@gmail.com>
2024-04-18 04:46:47 +0800

    add validation type with zod

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,7 +6,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function formatMonry(amount: number) {
+export function formatMoney(amount: number) {
   return new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
