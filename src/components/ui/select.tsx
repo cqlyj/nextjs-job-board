b7d468be7eaa8a51c37ff7579f489f72996651f9
cqlyj <luoyingjie0721@gmail.com>
2024-04-18 14:34:33 +0800

    fix issue in the style of jobFilter

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -10,7 +10,7 @@ export default forwardRef<
     <div className="relative">
       <select
         className={cn(
-          "h-10 w-full border rounded-md appearance-none truncate bg-background border-input py-2 pl-3 pr-8 text-sm ring-offset-background focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled: opacity-50",
+          "h-10 w-full border rounded-md appearance-none truncate bg-background border-input py-2 pl-3 pr-8 text-sm ring-offset-background focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
           className,
         )}
         ref={ref}
