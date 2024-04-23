c0964bba2241b6c5ff09067feb6f0e5a1622b5bf
cqlyj <luoyingjie0721@gmail.com>
2024-04-23 17:24:00 +0800

    Add remotePatterns to images configuration in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+import { hostname } from "os";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "nmt1v2goas2uqoaa.public.blob.vercel-storage.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
