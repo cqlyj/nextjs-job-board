9f7acbb84a251c5fcfc0be1a4bc56de3b3f8d95b
cqlyj <luoyingjie0721@gmail.com>
2024-04-23 17:24:17 +0800

    Add job-submitted page component

---
+++ src/app/job-submitted/page.tsx
@@ -0,0 +1,12 @@
+import H1 from "@/components/ui/h1";
+
+const page = () => {
+  return (
+    <main className="m-auto max-w-5xl space-y-5 px-3 text-center">
+      <H1>Job Submitted</H1>
+      <p>Your job posting has been submitted and is pending approval</p>
+    </main>
+  );
+};
+
+export default page;
