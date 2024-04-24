a15799f40934ee79930470324281303df22e1968
cqlyj <luoyingjie0721@gmail.com>
2024-04-24 13:48:03 +0800

    Add NotFound component for handling 404 errors

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import H1 from "@/components/ui/h1";
+
+const NotFound = () => {
+  return (
+    <main className="m-auto my-10 max-w-5xl space-y-5 px-3 text-center">
+      <H1>Not Found</H1>
+      <p>Sorry, the page is not found.</p>
+    </main>
+  );
+};
+
+export default NotFound;
