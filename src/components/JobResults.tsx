6ab3b108062e02e2fcd868729b5ba01cdc9dc720
cqlyj <luoyingjie0721@gmail.com>
2024-04-24 13:48:08 +0800

    Add link to job details page in JobResults component

--- src/components/JobResults.tsx
+++ src/components/JobResults.tsx
@@ -2,6 +2,7 @@ import prisma from "@/lib/prisma";
 import JobListItem from "./JobListItem";
 import { JobFilterValues } from "@/lib/validation";
 import { Prisma } from "@prisma/client";
+import Link from "next/link";
 
 interface JobResultsProps {
   filterValues: JobFilterValues;
@@ -46,7 +47,9 @@ const JobResults = async ({
   return (
     <div className="flex-grow space-y-4">
       {jobs.map((job) => (
-        <JobListItem job={job} key={job.id} />
+        <Link key={job.id} href={`/jobs/${job.slug}`} className="block">
+          <JobListItem job={job} />
+        </Link>
       ))}
       {jobs.length === 0 && (
         <p className="m-auto text-center">
