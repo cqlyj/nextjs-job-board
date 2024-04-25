84c86011b9b7bb787854f0b63ba5981672071e9e
cqlyj <luoyingjie0721@gmail.com>
2024-04-25 13:31:28 +0800

    Add page component for displaying job details

---
+++ src/app/admin/jobs/[slug]/page.tsx
@@ -0,0 +1,27 @@
+import JobPage from "@/components/JobPage";
+import prisma from "@/lib/prisma";
+import { notFound } from "next/navigation";
+import AdminSidebar from "./AdminSidebar";
+
+interface PageProps {
+  params: {
+    slug: string;
+  };
+}
+
+const page = async ({ params: { slug } }: PageProps) => {
+  const job = await prisma.job.findUnique({
+    where: { slug },
+  });
+
+  if (!job) notFound();
+
+  return (
+    <main className="flex m-auto my-10 max-w-5xl flex-col items-center gap-5 px-3 md:flex-row md:items-start">
+      <JobPage job={job}></JobPage>
+      <AdminSidebar job={job}></AdminSidebar>
+    </main>
+  );
+};
+
+export default page;
