dc619eb244eadf596401402b0af031a79f2ed32e
cqlyj <luoyingjie0721@gmail.com>
2024-04-25 13:31:24 +0800

    Update admin layout to include AdminNavbar component

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
+import AdminNavbar from "./jobs/[slug]/AdminNavbar";
 
 export const metadata = {
   title: "Admin",
 };
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  return <ClerkProvider>{children}</ClerkProvider>;
+  return (
+    <ClerkProvider>
+      <AdminNavbar></AdminNavbar>
+      {children}
+    </ClerkProvider>
+  );
 };
 
 export default Layout;
